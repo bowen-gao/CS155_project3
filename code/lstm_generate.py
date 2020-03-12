33482b0dbc3a9212025fce9272e6e33d93fc2c20
gaobowen <shixi_bowen16@staff.sina.com.cn>
2020-03-11 20:25:26 -0700

    checkpoint

--- code/lstm_generate.py
+++ code/lstm_generate.py
@@ -2,7 +2,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
 import numpy as np
 import tensorflow as tf
-
+import random
 import re
 
 embed_dim = 128
@@ -16,23 +16,11 @@ X = []
 Y = []
 
 num = len(set(text))
-dic = {}
+char2index = {}
+index2char = {}
 for i, char in enumerate(list(set(text))):
-    dic[char] = i
-text_list = []
-for char in text:
-    tmp = [0] * num
-    tmp[dic[char]] = 1
-    text_list.append(tmp)
-for i in range(0, len(text) - 40, 1):
-    print(i)
-    seq = text_list[i:i + 40]
-    label = text_list[i + 40]
-    X.append(seq)
-    Y.append(label)
-
-X = np.array(X)
-Y = np.array(Y)
+    char2index[char] = i
+    index2char[i] = char
 
 model = Sequential()
 # model.add(Embedding(num, embed_dim, input_length=40))
@@ -42,16 +30,30 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 print(model.summary())
 
 # X nx40
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath="checkpoints/model.h5",
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath="checkpoints/cp.ckpt",
                                                  save_weights_only=True,
                                                  verbose=1)
 
 
 def genertate(model):
-    pass
+    text = "shall i compare thee to a summer's day?\n"
+    for i in range(10):
+        x = []
+        for j in range(40):
+            tmp = [0] * num
+            index = char2index[text[i + j]]
+            tmp[index] = 1
+            x.append(tmp)
+        x = np.array(x).reshape((1, len(x), len(x[1])))
+        y = model.predict(x)
+        index = random.choices([i for i in range(num)], y[0])[0]
+        char = index2char[index]
+        text += char
+    print(text)
 
 
 if __name__ == '__main__':
-    print(X.shape, Y.shape)
-    model.fit(X, Y, epochs=100, callbacks=[cp_callback])
-    model.save("models/lstm_model")
+    model.load_weights("checkpoints/cp.ckpt")
+    # print(model.evaluate(X, Y, verbose=1))
+    # model.save("models/lstm_model")
+    genertate(model)
